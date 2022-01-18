dd474f056a31e02d241603e07e0cc847edb12d1b
eaglesakura <eagle.sakura@gmail.com>
2022-01-18 09:11:22 +0900

    add: DirectoryExtensions

---
+++ lib/src/directory_extensions.dart
@@ -0,0 +1,17 @@
+import 'dart:io' as io;
+
+extension DirectoryExtensions on io.Directory {
+  /// ディレクトリの削除を試す。成功した場合はtrue, そうでない場合はfalseを返却する.
+  /// 既に削除されている場合は成功とみなし、trueを返却する.
+  Future<bool> tryDelete({required bool recursive}) async {
+    try {
+      if (!await exists()) {
+        return true;
+      }
+      await delete(recursive: recursive);
+      return true;
+    } on Exception catch (_) {
+      return false;
+    }
+  }
+}
