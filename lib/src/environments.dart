b6ae5dbf0852732fd9f88e21fc1424d29b7529c1
eaglesakura <eagle.sakura@gmail.com>
2022-01-26 20:16:02 +0900

    fix: 'kIsWeb' == trueのときPlatformへのアクセスでクラッシュする問題を修正

--- lib/src/environments.dart
+++ lib/src/environments.dart
@@ -1,9 +1,11 @@
 import 'dart:io' as io;
 
+import 'package:flutter/foundation.dart';
+
 class Environments {
   Environments._();
 
   /// FlutterのUnitTest($ flutter test)として実行中であればtrueを返却する.
   static bool get isFlutterTesting =>
-      io.Platform.environment.containsKey('FLUTTER_TEST');
+      !kIsWeb && io.Platform.environment.containsKey('FLUTTER_TEST');
 }
